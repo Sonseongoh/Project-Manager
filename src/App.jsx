cf68d2be04b38da46cdc3b3b524c544b4683b4e6
Sonseongoh <nobleser@naver.com>
2024-03-01 02:11:39 +0900

    프로젝트 매니저

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    //null일경우 새로운 프로젝트, undefined일경우 선택되지 않음 ,id입력일 경우 해당 프로젝트 선택
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId, //혹시 중복될 가능성이 있으나 작은규모의 프로젝트여서 일단 패스
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,22 @@
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button";
+export default function NoProjectSelected({ onStartAddProject }) {
+  return (
+    <div className="mt-24 text-center w-2/3 ">
+      <img
+        src={noProjectImage}
+        alt="An empty task list"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 mt-4 my-4">
+        프로젝트가 선택되지 않음
+      </h2>
+      <p className="text-stone-400 mb-4">
+        프로젝트를 선택하거나 새로운 프로젝트를 시작하세요
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>새 프로젝트 생성</Button>
+      </p>
+    </div>
+  );
+}
